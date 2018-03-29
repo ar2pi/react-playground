d5e0f884356a33b70fd179a38cd4ade4a13d2f60
Arthur Picerna <arthurpicerna@gmail.com>
2018-03-28 22:22:18 -0500

    react-redux

--- expensify/src/components/EditExpensePage.js
+++ expensify/src/components/EditExpensePage.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-    console.log(props);
-    return (
-        <div>Valid component 3, id: {props.match.params.id}</div>
-    );
+const EditExpensePage = (props) => (
+    <div>
+        <ExpenseForm
+            expense={props.expense}
+            onSubmit={(expense) => {
+                props.dispatch(editExpense(props.expense.id, expense));
+                props.history.push('/');
+            }}
+        />
+        <button
+            onClick={(e) => {
+                props.dispatch(removeExpense({ id: props.expense.id }));
+                props.history.push('/');
+            }}
+        >
+            Remove
+        </button>
+    </div>
+);
+
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    };
 };
 
-export default EditExpensePage;
+export default connect(mapStateToProps)(EditExpensePage);
